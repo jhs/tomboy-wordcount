376e472cf0cf6a5480a2337655cd16d0523a8043
Jason Smith <jhs@proven-corporation.com>
2008-07-26 18:37:03 +0700

    Whitespace corrections to match the Tomboy coding style

--- WordcountNoteAddin.cs
+++ WordcountNoteAddin.cs
@@ -40,74 +40,76 @@ namespace Tomboy.Wordcount
 
 		public override void OnNoteOpened ()
 		{
-                    // Add the menu item when the window is created.
-                    menu_item = new Gtk.ImageMenuItem(Catalog.GetString("Word count"));
-                    menu_item.Image = new Gtk.Image (Gtk.Stock.Index, Gtk.IconSize.Menu);  /* TODO: correct this */
-                    menu_item.Activated += OnMenuItemActivated;
-                    menu_item.AddAccelerator("activate", Window.AccelGroup,
-                                             (uint) Gdk.Key.o, Gdk.ModifierType.ControlMask,
-                                             Gtk.AccelFlags.Visible);
+			// Add the menu item when the window is created.
+			menu_item = new Gtk.ImageMenuItem(Catalog.GetString("Word count"));
+			menu_item.Image = new Gtk.Image (Gtk.Stock.Index, Gtk.IconSize.Menu);  /* TODO: correct this */
+			menu_item.Activated += OnMenuItemActivated;
+			menu_item.AddAccelerator("activate", Window.AccelGroup,
+				(uint) Gdk.Key.o, Gdk.ModifierType.ControlMask,
+				Gtk.AccelFlags.Visible);
 
-                    menu_item.Show ();
-                    AddPluginMenuItem (menu_item);
+			menu_item.Show ();
+			AddPluginMenuItem (menu_item);
 		}
 
-                static Regex wordSplitter = new Regex("\\s+");
+		static Regex wordSplitter = new Regex("\\s+");
 
-                /* Return the word count of an arbitrary multi-line text string. */
-                private int CountWords (string s)
-                {
-                    return wordSplitter.Split(s).Length;
-                }
+		/* Return the word count of an arbitrary multi-line text string. */
+		private int CountWords (string s)
+		{
+			return wordSplitter.Split(s).Length;
+		}
 
 		void OnMenuItemActivated (object sender, EventArgs args)
 		{
-                        int lines, words, chars;    // Like wc.
+			int lines, words, chars;// Like wc.
 
-                        // For now, the char count simply omits the top-level XML tag.
-                        string header = "<note-content version=\"0.1\">";
-                        string footer = "</note-content>";
+			// For now, the char count simply omits the top-level XML tag.
+			string header = "<note-content version=\"0.1\">";
+			string footer = "</note-content>";
 
-                        chars = Note.Data.Text.Length - header.Length - footer.Length;
-                        string realText = Note.Data.Text.Substring(header.Length, chars);
+			chars = Note.Data.Text.Length - header.Length - footer.Length;
+			string realText = Note.Data.Text.Substring(header.Length, chars);
 
-                        /* The word count is straightforward, but remember to omit the note title. */
-                        words = CountWords(realText);
-                        words -= CountWords(Note.Title);
+			/* The word count is straightforward, but remember to omit the note title. */
+			words = CountWords(realText);
+			words -= CountWords(Note.Title);
 
-                        lines = realText.Split('\n').Length;
-                        lines -= 2; // Omit the Title and blank line beneath it.
+			lines = realText.Split('\n').Length;
+			lines -= 2; // Omit the Title and blank line beneath it.
 
-                        ShowStats(Note.Title, lines, words, chars);
+			ShowStats(Note.Title, lines, words, chars);
 		}
 
-                private Gtk.Window statWin;
+		private Gtk.Window statWin;
 
-                private void ShowStats(string title, int lines, int words, int chars)
-                {
-                    Logger.Log("Wordcount: {0}: {1} {2} {3}", title, lines, words, chars);
+		private void ShowStats (string title, int lines, int words, int chars)
+		{
+			Logger.Log("Wordcount: {0}: {1} {2} {3}", title, lines, words, chars);
 
-                    statWin = new Gtk.Window(String.Format("{0} - Word count", title));
-                    statWin.Resize(200, 100);
+			statWin = new Gtk.Window(String.Format("{0} - Word count", title));
+			statWin.Resize(200, 100);
 
-                    Gtk.VBox box = new Gtk.VBox(false, 0);
+			Gtk.VBox box = new Gtk.VBox(false, 0);
 
-                    Gtk.Label statLabel = new Gtk.Label();
-                    statLabel.Text = String.Format("{0}\n\nLines: {1}\nWords: {2}\nCharacters: {3}\n", title, lines, words, chars);
+			Gtk.Label statLabel = new Gtk.Label();
+			statLabel.Text = String.Format("{0}\n\nLines: {1}\nWords: {2}\nCharacters: {3}\n", title, lines, words, chars);
 
-                    box.PackStart(statLabel, true, true, 0);
+			box.PackStart(statLabel, true, true, 0);
 
-                    Gtk.Button ok = new Gtk.Button("Close");
-                    ok.Clicked += new EventHandler(OkHandler);
-                    box.PackStart(ok, true, true, 0);
+			Gtk.Button ok = new Gtk.Button("Close");
+			ok.Clicked += new EventHandler(OkHandler);
+			box.PackStart(ok, true, true, 0);
 
-                    statWin.Add(box);
-                    statWin.ShowAll();
-                }
+			statWin.Add(box);
+			statWin.ShowAll();
+		}
 
-                private void OkHandler(object o, EventArgs args)
-                {
-                    statWin.Destroy();
-                }
+		private void OkHandler (object o, EventArgs args)
+		{
+			statWin.Destroy();
+		}
 	}
 }
+
+// vim: sts=0 ts=8 sw=8 noet
--- env.sh
+++ env.sh
@@ -9,23 +9,25 @@
 tomboy_prefix_parent='/usr/local/software'
 
 if [ $(basename "$0") = 'env.sh' ]; then
-    echo "This program should be sourced, not run standalone" >&2
-    exit 1
+	echo "This program should be sourced, not run standalone" >&2
+	exit 1
 fi
 
 # Maybe it's already here.
 if pkg-config --exists tomboy-addins; then
-    echo "Environment is okay.  Using: `pkg-config --variable=prefix tomboy-addins`"
+	echo "Environment is okay.  Using: `pkg-config --variable=prefix tomboy-addins`"
 else
-    # Otherwise, search for it.
-    for app in $(find "$tomboy_prefix_parent" -type d -wholename '*/lib/pkgconfig' 2> /dev/null); do
-        new_path="$app:$PKG_CONFIG_PATH"
-        if env PKG_CONFIG_PATH="$new_path" pkg-config --exists tomboy-addins; then
-            echo "Found Tomboy: $app"
-            export PKG_CONFIG_PATH="$new_path"
-            break
-        else
-            echo "no: $app"
-        fi
-    done
+	# Otherwise, search for it.
+	for app in $(find "$tomboy_prefix_parent" -type d -wholename '*/lib/pkgconfig' 2> /dev/null); do
+		new_path="$app:$PKG_CONFIG_PATH"
+		if env PKG_CONFIG_PATH="$new_path" pkg-config --exists tomboy-addins; then
+			echo "Found Tomboy: $app"
+			export PKG_CONFIG_PATH="$new_path"
+			break
+		else
+			echo "no: $app"
+		fi
+	done
 fi
+
+# vim: sts=0 ts=8 sw=8 noet
