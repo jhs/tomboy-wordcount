8de60fe9aae72acbb37a220dc3e96da34bb14c25
Jason Smith <jhs@proven-corporation.com>
2008-07-26 17:52:53 +0700

    Document and generalize the environment script for others to reconfigure it if necessary

--- env.sh
+++ env.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
-# Set the pkg-config environment
+# Set the pkg-config environment.  This script should only be sourced if you installed
+# Tomboy in a non-standard location (via the --prefix argument to configure).
+
+# Set this to the *parent* of the prefix where you installed tomboy.  For example,
+# if your prefix is /usr/local then this should be "/usr".  (Although I myself prefer
+# /usr/local/software/tomboy because I like to use GNU stow.)
+tomboy_prefix_parent='/usr/local/software'
 
 if [ $(basename "$0") = 'env.sh' ]; then
     echo "This program should be sourced, not run standalone" >&2
@@ -12,8 +18,7 @@ if pkg-config --exists tomboy-addins; then
     echo "Environment is okay.  Using: `pkg-config --variable=prefix tomboy-addins`"
 else
     # Otherwise, search for it.
-    base='/usr/local/software'
-    for app in $(find "$base" -type d -wholename '*/lib/pkgconfig' 2> /dev/null); do
+    for app in $(find "$tomboy_prefix_parent" -type d -wholename '*/lib/pkgconfig' 2> /dev/null); do
         new_path="$app:$PKG_CONFIG_PATH"
         if env PKG_CONFIG_PATH="$new_path" pkg-config --exists tomboy-addins; then
             echo "Found Tomboy: $app"
